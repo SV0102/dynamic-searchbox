ec19f362e691f7553fd600a0cf851da3aceca524
SV <Sergej@sergej.com>
2022-04-20 17:33:16 +0300

    feat: very early page markup was made

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from "react";
 import "./App.css";
 import { Button } from "@rmwc/button";
-import { Grid } from "@rmwc/grid";
-
-import { GridCell } from "@rmwc/grid";
 import { TextField } from "@rmwc/textfield";
+import { TabBar } from "@rmwc/tabs";
+import { Tab } from "@rmwc/tabs";
+import { Grid } from "@rmwc/grid";
+import { GridCell } from "@rmwc/grid";
+import { Select } from "@rmwc/select";
+
+import "@material/select/dist/mdc.select.css";
+import "@material/floating-label/dist/mdc.floating-label.css";
+import "@material/notched-outline/dist/mdc.notched-outline.css";
+import "@material/line-ripple/dist/mdc.line-ripple.css";
+import "@material/list/dist/mdc.list.css";
+import "@material/menu/dist/mdc.menu.css";
+import "@material/menu-surface/dist/mdc.menu-surface.css";
 
 import "@material/button/dist/mdc.button.css";
+
 import "@material/layout-grid/dist/mdc.layout-grid.css";
+
 import "@material/textfield/dist/mdc.textfield.css";
 import "@material/floating-label/dist/mdc.floating-label.css";
 import "@material/notched-outline/dist/mdc.notched-outline.css";
 import "@material/line-ripple/dist/mdc.line-ripple.css";
+
+import "@material/tab-bar/dist/mdc.tab-bar.css";
+import "@material/tab/dist/mdc.tab.css";
+import "@material/tab-scroller/dist/mdc.tab-scroller.css";
+import "@material/tab-indicator/dist/mdc.tab-indicator.css";
 
 function App() {
   return (
@@ -21,34 +38,69 @@ function App() {
         <Button>All</Button>
         <Button>Contacts</Button>
         <Button>Clauses</Button>
-        {/*<Grid align={'right'} fixedColumnWidth={undefined}>*/}
-        {/*  <GridCell span={2} align={'middle'}>Drunk finder</GridCell>*/}
-        {/*  <GridCell span={1}><Button>Ideas</Button></GridCell>*/}
-        {/*  <GridCell span={1}><Button>Plans</Button></GridCell>*/}
-        {/*  <GridCell span={2}><Button>Clauses</Button></GridCell>*/}
-        {/*  <GridCell span={2}><Button>Click</Button></GridCell>*/}
-        {/*</Grid>*/}
       </header>
       <nav className={"navy_nav"}>
-        <span className={"text_field"}>
-          <TextField outlined placeholder="Enter your question" />
-        </span>
-        <Button>Search</Button>
-        <Grid>
-          <GridCell span={8}>
-            <TextField fullwidth={true} placeholder="Enter your question" />
-          </GridCell>
-          <GridCell>
-            <Button>Search</Button>
-          </GridCell>
-        </Grid>
+        <TextField
+          placeholder="Enter your question"
+          style={{
+            width: "35rem",
+            background: "white",
+            margin: 15,
+          }}
+        />
+        <Button raised>Search</Button>
       </nav>
-      <nav>
-        <Button>All</Button>
-        <Button>Contacts</Button>
-        <Button>Clauses</Button>
-        <Button>Areas</Button>
-      </nav>
+      <Grid>
+        <GridCell span={6}>
+          <TabBar>
+            <Tab>All</Tab>
+            <Tab>Contacts</Tab>
+            <Tab>Clauses</Tab>
+            <Tab>Areas</Tab>
+          </TabBar>
+        </GridCell>
+        <GridCell></GridCell>
+      </Grid>
+
+      <Grid>
+        <GridCell span={3}>
+          <p className={"filters"}>Filters         (All)</p>
+          <Select
+            enhanced
+            options={["Europe", "Asia", "North America", "South America",
+              "Australia"]}
+            placeholder={"Choose region"}
+            style={{ margin: "10px" }}
+          />
+          <Select
+            enhanced
+            options={["country1", "country2", "country3"]}
+            placeholder={"Choose country"}
+          />
+          <Select
+            enhanced
+            options={["region1", "region2", "region3"]}
+            placeholder={"Choose region"}
+            style={{ margin: "10px" }}
+          />
+          <Select
+            enhanced
+            options={["city1", "city2", "city3"]}
+            placeholder={"Choose city"}
+            style={{ margin: "10px" }}
+          />
+          <br />
+        </GridCell>
+        <GridCell span={8}>
+          <main>
+            Here will be some applied filters
+            <br />
+            -------------------------
+            <br />
+            Results
+          </main>
+        </GridCell>
+      </Grid>
     </>
   );
 }
